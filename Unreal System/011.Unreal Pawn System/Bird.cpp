b1ef11873045422a328a7ed37c7163025bd3c442
Shin-Hyeong <55770838+Shin-Hyeong@users.noreply.github.com>
2023-11-07 20:33:55 +0900

    Add Movement Input

--- Unreal System/011.Unreal Pawn System/Bird.cpp
+++ Unreal System/011.Unreal Pawn System/Bird.cpp
@@ -1,12 +1,15 @@
 #include "Pawn/Bird.h"
 #include "Components/CapsuleComponent.h"
 #include "components/SkeletalMeshComponent.h"
+#include "Components/InputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
 
 ABird::ABird()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
-	// Capsule 객체 생성
 	Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
 	// Y축(세로) 길이 반절 설정
 	Capsule->SetCapsuleHalfHeight(20.f);
@@ -17,28 +20,71 @@ ABird::ABird()
 
 	// SkeletalMesh 생성
 	BirdMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("BirdMesh"));
-	// BirdMesh를 RootComponent 하위로 설정.
 	BirdMesh->SetupAttachment(GetRootComponent());
 
+	// Pawn에 Controller와 연결
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
 }
+
 
 void ABird::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem *Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(MappingContext, 0);
+			UE_LOG(LogTemp, Warning, TEXT("MappingContext!"));
+		}
+	}
 	
 }
 
-// Called every frame
 void ABird::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
-// Called to bind functionality to input
 void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	if (UEnhancedInputComponent *EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		// Binding
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABird::Move);
+	}
 }
 
+void ABird::Move(const FInputActionValue &Value)
+{
+	const FVector2D MovementValue = Value.Get<FVector2D>();
+	if (Controller != nullptr)
+	{
+		// Rotator 가져옴.
+		const FRotator Rotation = Controller->GetControlRotation();
+		// Rotator의 Yaw만 가져옴.
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// YawRotator의 X 축 변수
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		// YawRotator의 Y 축 변수
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		AddMovementInput(ForwardDirection, MovementValue.Y);
+		AddMovementInput(RightDirection, MovementValue.X);
+
+		// Debuging
+		if (MovementValue.Y > 0)
+			GEngine->AddOnScreenDebugMessage(1, 2.0f, FColor::Yellow, TEXT("Forward"));
+		else if (MovementValue.Y < 0)
+			GEngine->AddOnScreenDebugMessage(1, 2.0f, FColor::Yellow, TEXT("Back"));
+		if (MovementValue.X > 0)
+			GEngine->AddOnScreenDebugMessage(1, 2.0f, FColor::Yellow, TEXT("Right"));
+		else if (MovementValue.X < 0)
+			GEngine->AddOnScreenDebugMessage(1, 2.0f, FColor::Yellow, TEXT("Left"));
+	}
+}
--- Unreal System/011.Unreal Pawn System/Bird.h
+++ Unreal System/011.Unreal Pawn System/Bird.h
@@ -4,19 +4,31 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "InputActionValue.h"
+#include "GameFramework/Controller.h"
 #include "Bird.generated.h"
 
 class UCapsuleComponent;
 class USkeletalMeshComponent;
+class UInputAction;
+class UInputMappingContext;
 
-UCLASS()
-class UDEMYCPP_API ABird : public APawn
+UCLASS() class UDEMYCPP_API ABird : public APawn
 {
 	GENERATED_BODY()
 
 public:
 	// Sets default values for this pawn's properties
 	ABird();
+
+	// Input Action
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Action")
+	UInputAction *MoveAction;
+
+	// InputMappingContext
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Action")
+	UInputMappingContext *MappingContext;
+
 
 private:
 	// 충돌을 감지할 Capsule
@@ -30,6 +42,8 @@ private:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	void Move(const FInputActionValue& Value);
 
 public:	
 	// Called every frame
