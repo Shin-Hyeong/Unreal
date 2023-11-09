dc39b6a91439bc16e10e508c963ef872328d32e8
Shin-Hyeong <55770838+Shin-Hyeong@users.noreply.github.com>
2023-11-09 22:58:53 +0900

    Camera Rotation

--- Unreal System/011.Unreal Pawn System/Bird.cpp
+++ Unreal System/011.Unreal Pawn System/Bird.cpp
@@ -1,7 +1,11 @@
 #include "Pawn/Bird.h"
+
 #include "Components/CapsuleComponent.h"
 #include "components/SkeletalMeshComponent.h"
 #include "Components/InputComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
 
@@ -10,6 +14,7 @@ ABird::ABird()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Capsule Collision 객체 생성
 	Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule"));
 	// Y축(세로) 길이 반절 설정
 	Capsule->SetCapsuleHalfHeight(20.f);
@@ -21,6 +26,20 @@ ABird::ABird()
 	// SkeletalMesh 생성
 	BirdMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("BirdMesh"));
 	BirdMesh->SetupAttachment(GetRootComponent());
+
+	// SpringArm 객체 생성
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	// RootComponent(Capsule)에 종속 시킴
+	CameraBoom->SetupAttachment(GetRootComponent());
+	// SpringArm의 길이 조절
+	CameraBoom->TargetArmLength = 300.f;
+	
+	// Camera 객체 생성
+	ViewCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("ViewCamera"));
+	// SpringArm에 Camera 종속시킴.
+	ViewCamera->SetupAttachment(CameraBoom);
+
+	// BP에서 bUseControllerPitch(컨트롤러 회전 Pitch 사용), bUseControllerYaw(컨트롤러 회전 Yaw 사용)를 true로 해주어야 회전 됨.
 
 	// Pawn에 Controller와 연결
 	AutoPossessPlayer = EAutoReceiveInput::Player0;
@@ -56,35 +75,32 @@ void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 	{
 		// Binding
 		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ABird::Move);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ABird::Look);
 	}
 }
 
 void ABird::Move(const FInputActionValue &Value)
 {
-	const FVector2D MovementValue = Value.Get<FVector2D>();
+	const float MovementValue = Value.Get<float>();
 	if (Controller != nullptr)
 	{
-		// Rotator 가져옴.
-		const FRotator Rotation = Controller->GetControlRotation();
-		// Rotator의 Yaw만 가져옴.
-		const FRotator YawRotation(0, Rotation.Yaw, 0);
+		// Pawn의 Forward Fector 구함.
+		FVector Forward = GetActorForwardVector();
 
-		// YawRotator의 X 축 변수
-		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-		// YawRotator의 Y 축 변수
-		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		// 입력 값에 따라 이동
+		AddMovementInput(Forward, MovementValue);
+	}
+}
 
-		AddMovementInput(ForwardDirection, MovementValue.Y);
-		AddMovementInput(RightDirection, MovementValue.X);
+void ABird::Look(const FInputActionValue& Value)
+{
+	// LookAction 입력값을 FVector2D 변수 값으로 받아옴.
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
 
-		// Debuging
-		if (MovementValue.Y > 0)
-			GEngine->AddOnScreenDebugMessage(1, 2.0f, FColor::Yellow, TEXT("Forward"));
-		else if (MovementValue.Y < 0)
-			GEngine->AddOnScreenDebugMessage(1, 2.0f, FColor::Yellow, TEXT("Back"));
-		if (MovementValue.X > 0)
-			GEngine->AddOnScreenDebugMessage(1, 2.0f, FColor::Yellow, TEXT("Right"));
-		else if (MovementValue.X < 0)
-			GEngine->AddOnScreenDebugMessage(1, 2.0f, FColor::Yellow, TEXT("Left"));
+	if (Controller != nullptr)
+	{
+		// 입력 값에 따라 이동
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y);
 	}
 }
--- Unreal System/011.Unreal Pawn System/Bird.h
+++ Unreal System/011.Unreal Pawn System/Bird.h
@@ -1,29 +1,33 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
-#include "InputActionValue.h"
 #include "GameFramework/Controller.h"
+#include "InputActionValue.h"
 #include "Bird.generated.h"
 
 class UCapsuleComponent;
 class USkeletalMeshComponent;
+class USpringArmComponent;
+class UCameraComponent;
+
 class UInputAction;
 class UInputMappingContext;
 
-UCLASS() class UDEMYCPP_API ABird : public APawn
+UCLASS() 
+class UDEMYCPP_API ABird : public APawn
 {
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this pawn's properties
 	ABird();
 
 	// Input Action
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Action")
 	UInputAction *MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Action")
+	UInputAction* LookAction;
 
 	// InputMappingContext
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Action")
@@ -39,11 +43,23 @@ private:
 	UPROPERTY(VisibleAnywhere)
 	USkeletalMeshComponent* BirdMesh;
 
+	// Spring Arm
+	// Spring Arm을 사용하는 이유
+	// - 1) 동적으로 카메라의 위치를 조절하기 쉬워짐
+	// - 2) 카메라가 벽을 만나면 벽을 통과하는게 아니라 객체 기준으로 확대됨.
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* ViewCamera;
+
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	// 이동 함수
 	void Move(const FInputActionValue& Value);
+	// 카메라 회전
+	void Look(const FInputActionValue& Value);
 
 public:	
 	// Called every frame
