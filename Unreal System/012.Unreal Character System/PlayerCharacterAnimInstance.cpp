913c4d072e1fa20a9834a6e031509a729c52fb08
Shin-Hyeong <55770838+Shin-Hyeong@users.noreply.github.com>
2023-11-13 23:01:10 +0900

    Player Animation Instance - AnimBP ParentsClass

---
+++ Unreal System/012.Unreal Character System/PlayerCharacterAnimInstance.cpp
@@ -0,0 +1,43 @@
+#include "Player/PlayerCharacterAnimInstance.h"
+
+// PlayerCharacter에 플레이어가 조종하는 객체를 참조 시키기 위해
+#include "Player/PlayerCharacter.h"
+// C++의 수학라이브러리
+#include "Kismet/KismetMathLibrary.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+
+// beginPlay()
+void UPlayerCharacterAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	// Dynamic_cast() : 상속의 안정성을 확인하는 Cast. 실패하면 nullptr
+	// C++의 Dynamic_cast<>()은 UE에서 Cast<>()으로 이루어진다.
+	// TryGetPawnOwner : APawn 타입을 반환함. 해당 Animation BP를 가지고 있는 Pawn를 알려줌.
+	// Cast<APlayerCharacter>(TryGetPawnOwner()) : 받은 Pawn을 APlayerCharacter 타입으로 변환
+	PlayerCharacter =  Cast<APlayerCharacter>(TryGetPawnOwner());
+
+	// PlayerCharacter가 nullptr이 아니면(cast이 성공하면)
+	if (PlayerCharacter)
+	{
+		// PlayerCharacter가 가지고 있는 이동구성요소를 가져옴.
+		// PlayerCharacter.cpp에서 "GameFramework/CharacterMovementComponent.h"를 참조하기 때문에 사용가능.
+		PlayerCharacterMovement = PlayerCharacter->GetCharacterMovement();
+	}
+}
+
+// Tick
+void UPlayerCharacterAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeUpdateAnimation(DeltaTime);
+
+	// PlayerCharacterMovement이 nullptr이 아니면
+	if (PlayerCharacterMovement)
+	{
+		// VSizeXY : X와 Y값만 가진 Vector의 Length를 반환함
+		GroundSpeed = UKismetMathLibrary::VSizeXY(PlayerCharacterMovement->Velocity);
+		// 플레이어가 공중에 떠있는지(점프 중인지) 확인
+		IsFalling = PlayerCharacterMovement->IsFalling();
+	}
+}
---
+++ Unreal System/012.Unreal Character System/PlayerCharacterAnimInstance.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "PlayerCharacterAnimInstance.generated.h"
+
+/*
+AnimInstance는 Animation BP를 부모로 하는 클래스이다.
+AnimationBP는 게임이 실행되지 않더라도 계속 실시간으로 반응하기 한다.
+AnimInstance를 작업 중 nullptr같은 에러 사항을 실시간으로 반응하기 때문에
+UE 에디터가 크래시를 일으킬 수 있기 때문에 AnimInstance를 작업할 때는 에디터를 끄고 작업한다.
+
+AnimInstance 변경은 ABP - ClassSetting - Detail에서 변경가능
+ */
+
+UCLASS()
+class UDEMYCPP_API UPlayerCharacterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+public:
+	// 플레이어의 class이름으로 생성해야함.
+	UPROPERTY(BlueprintReadOnly)
+	class APlayerCharacter* PlayerCharacter;
+
+	// 플레이어의 Movement 
+	UPROPERTY(BlueprintReadOnly, Category = Movement)
+	class UCharacterMovementComponent* PlayerCharacterMovement;
+
+	// 플레이어의 이동속도
+	UPROPERTY(BlueprintReadOnly, Category = Movement)
+	float GroundSpeed;
+
+	// 플레이어가 공중에 있는지 확인
+	UPROPERTY(BlueprintReadOnly, Category = Movement)
+	bool IsFalling;
+
+public:
+	// beginplay()
+	virtual void NativeInitializeAnimation() override;
+	// Tick()
+	virtual void NativeUpdateAnimation(float DeltaTime) override;
+};
