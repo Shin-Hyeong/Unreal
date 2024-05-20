53b8405b60cf39b0d2ce510ccee205cdc1d1df8d
Shin-Hyeong <55770838+Shin-Hyeong@users.noreply.github.com>
2024-05-20 19:29:52 +0900

    LineTrace from Crosshair&Muzzle / Zoom / OffsetYaw
    - ShooterCharacter
    Crosshair로 부터의 LineTrace와 Muzzle로 부터의 LineTrace의 차이로 인한 오류 수정
    사격 전 ZoomIn(견착) 기능
    줌 모션
    
    LineTrace Function Refactoring
    
    - ShooterAnimInstance
    캐릭터의 전방 FVector기준으로 캐릭터 이동할려는 FVector 방향 구하기(MovementOffsetYaw)
    MovementOffsetYaw를 통해서 JogStart, Jog, JogStop을 각 방향에 맞는 BlendSpace 적용
    
    줌하고 있는 모션과 총기 반동을 모션을 블렌딩

---
+++ Unreal - Shooter/001. Aiming fire by Crosshair in HUD/Character/002.Fire LineTrace & Zoom/Character/ShooterAnimInstance.cpp
@@ -0,0 +1,75 @@
+#include "Character/ShooterAnimInstance.h"
+
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+#include "Character/ShooterCharacter.h"
+
+void UShooterAnimInstance::NativeInitializeAnimation()
+{
+	// 해당 AnimInstacne를 가지고 있는 Pawn를 가져와서 ShooterCharacter로 캐스팅
+	SetShooterCharacter(); 
+}
+
+void UShooterAnimInstance::UpdateAnimationProperties(float DeltaTime)
+{
+	SetShooterCharacter(); 
+
+	SetAnimProperties();
+
+	if (ShooterCharacter)
+	{
+		// 캐릭터가 바라는 보는 방향
+		FRotator AimRotation = ShooterCharacter->GetBaseAimRotation();
+
+		// 월드 좌표 X방향을 기준으로 캐릭터의 이동할려는 Rotation값 만듦
+		FRotator MovementRoation = UKismetMathLibrary::MakeRotFromX(ShooterCharacter->GetVelocity());
+
+		// 캐릭터의 전방 FVector와 캐릭터가 이동할려는 방향 FVector의 차이 구함.
+		// NormalizedDeltaRotator : Normalized (A - B)
+		// 0 : Forward / 90 : Right / -90 : Left / -180(180) : Back
+		MovementOffsetYaw = UKismetMathLibrary::NormalizedDeltaRotator(MovementRoation, AimRotation).Yaw;
+
+		if (ShooterCharacter->GetVelocity().Size())
+		{
+			LastMovementOffsetYaw = MovementOffsetYaw;
+		}
+
+		bAiming = ShooterCharacter->GetAiming();
+
+		FString Message = FString::Printf(TEXT("Movement Offset : %f"), MovementOffsetYaw);
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(1, 0.f, FColor::White, Message);	
+		}
+	}
+}
+
+void UShooterAnimInstance::SetShooterCharacter()
+{
+	if (ShooterCharacter == nullptr)
+	{
+		ShooterCharacter = Cast<AShooterCharacter>(TryGetPawnOwner());
+		if (ShooterCharacter)
+			ShooterMovement = ShooterCharacter->GetCharacterMovement();
+	}
+}
+
+void UShooterAnimInstance::SetAnimProperties()
+{
+	if (ShooterCharacter)
+	{
+		Speed = UKismetMathLibrary::VSizeXY(ShooterMovement->Velocity);
+		bIsJumping = ShooterMovement->IsFalling();
+
+		// GetCurrentAccleration() : Get 현재 가속도 
+		if (ShooterMovement->GetCurrentAcceleration().Size() > 0.f)
+		{
+			bIsAccelerating = true;
+		}
+		else
+		{
+			bIsAccelerating = false;
+		}
+	}
+}
