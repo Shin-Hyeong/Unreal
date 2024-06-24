0552258ace4791bf295ca5e0341755ede5babb64
Shin-Hyeong <55770838+Shin-Hyeong@users.noreply.github.com>
2024-06-24 20:35:37 +0900

    Multiple Location Interpolation by Item Type
    - Item
    	enum ItemType을 만들어 Ammo인지 Weapon인지 분별함.
    	Item을 연속적으로 얻어서 Interpolation을 할 때 InterpLocations에서 ItemCount가 가장 적은 Index를 구하고
    	해당 Index가 가진  SceneComp로 Interp 시킴, Item Count +1, Interp가 끝나면 ItemCount - 1
    	ItemInterp에서 카메라의 위치를 가져오지 않고 이제 InterpLocations 배열을 통해 ItemType에 맞게 Interp 위치를 지정
    
    - Ammo
    	Ammo Asset 추가
    	AmmoMesh가 ItemState에 상태에 따라 설정될 수 있도록 함.
    	Ammo의 총알 수 만큼 Player가 가지고 있는 AmmoMap에 추가함.
    	Ammo Widget를 생성. LineTrace를 통해 Ammo를 바라보면 Widget 팝업
    	AmmoCollision를 Player가 Overlap 하면 자동 획득
    	
    - Player
    	Interpolation을 하기 위한 SceneComponent 생성
    	Interpolation을 하기 위한 SceneCompoent를 Get, 제어하기 위한 FInterpLocation 구조체 생성 및 초기화
    	FInterpLocation 구조체를 하나에 배열로 만든 InterpLocations 생성
    	Ammo & Weapon을 동시다발적으로 획득하면 소리가 겹치기 때문에 소리가 중복으로 재생되지 않도록 함.

---
+++ Unreal - Shooter/011.Multiple Location Interpolation/Items/Ammo.cpp
@@ -0,0 +1,97 @@
+#include "Items/Ammo.h"
+
+// Collision
+#include "Components/BoxComponent.h"
+#include "Components/SphereComponent.h"
+
+// Widget
+#include "Components/WidgetComponent.h"
+
+// Character
+#include "Character/ShooterCharacter.h"
+
+AAmmo::AAmmo()
+{
+	AmmoMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("AmmoMesh"));
+	SetRootComponent(AmmoMesh);
+
+	AmmoCollisionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("Ammo Collision Sphere"));
+	AmmoCollisionSphere->SetupAttachment(GetRootComponent());
+	AmmoCollisionSphere->SetSphereRadius(50.f);
+
+	GetBoxComponent()->SetupAttachment(GetRootComponent());
+	GetAreaSphere()->SetupAttachment(GetRootComponent());
+	GetPickupWidget()->SetupAttachment(GetRootComponent());
+}
+
+void AAmmo::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AAmmo::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AmmoCollisionSphere->OnComponentBeginOverlap.AddDynamic(this, &AAmmo::AmmoSphereCollisionOverlap);
+}
+
+void AAmmo::SetItemProperties(EItemState State)
+{
+	Super::SetItemProperties(State);
+
+	switch (State)
+	{
+	case EItemState::EIS_PickUp:
+		// 총알 메쉬 설정
+		AmmoMesh->SetSimulatePhysics(false);
+		AmmoMesh->SetVisibility(true);
+		AmmoMesh->SetEnableGravity(false);
+		AmmoMesh->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+		AmmoMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		break;
+
+	case EItemState::EIS_Equipped:
+		// 총알 메쉬 설정
+		AmmoMesh->SetSimulatePhysics(false);
+		AmmoMesh->SetVisibility(true);
+		AmmoMesh->SetEnableGravity(false);
+		AmmoMesh->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+		AmmoMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		break;
+
+	case EItemState::EIS_EquipInterping:
+		
+		// 총알 메쉬 설정
+		AmmoMesh->SetSimulatePhysics(false);
+		AmmoMesh->SetVisibility(true);
+		AmmoMesh->SetEnableGravity(false);
+		AmmoMesh->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+		AmmoMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		break;
+
+	case EItemState::EIS_Falling:
+		// 총알 메쉬 설정
+		AmmoMesh->SetSimulatePhysics(true);
+		AmmoMesh->SetEnableGravity(true);
+		AmmoMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+		AmmoMesh->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+		AmmoMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_WorldDynamic, ECollisionResponse::ECR_Block);
+		AmmoMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_WorldStatic, ECollisionResponse::ECR_Block);
+		break;
+	}
+}
+
+void AAmmo::AmmoSphereCollisionOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor)
+	{
+		AShooterCharacter* OverlapCharacter = Cast<AShooterCharacter>(OtherActor);
+		if (OverlapCharacter)
+		{
+			StartItemCurve(OverlapCharacter);
+			SetIsOnwer(true);
+			AmmoCollisionSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		}
+	}
+}
---
+++ Unreal - Shooter/011.Multiple Location Interpolation/Items/Ammo.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/Item.h"
+#include "AmmoType.h"
+#include "Ammo.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class SHOOTER_API AAmmo : public AItem
+{
+	GENERATED_BODY()
+public:
+	AAmmo();
+
+	/** <AActor> */ 
+	virtual void Tick(float DeltaTime) override;
+	/** </AActor> */
+
+	FORCEINLINE UStaticMeshComponent* GetAmmoMesh() const { return AmmoMesh; }
+	FORCEINLINE EAmmoType GetAmmoType() const { return AmmoType; }
+
+protected:
+	/** <AActor> */
+	virtual void BeginPlay() override;
+	/** </AActor> */
+
+	/** <AItem> */
+	// 픽업할때 AmmoMesh도 속성이 변경되도록 오버라이딩
+	virtual void SetItemProperties(EItemState State) override;
+	/** </AItem> */
+
+	UFUNCTION()
+	void AmmoSphereCollisionOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	
+private:
+	/**
+		Properties
+	*/
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Ammo | Item Properties", meta = (AllowPrivateAccess = "true"))
+	EAmmoType AmmoType{ EAmmoType::EAT_9mm };
+
+	/**
+		Collision
+	*/
+	// Player가 Overlap되면 자동으로 획득되도록 함.
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Ammo | Collision", meta = (AllowPrivateAccess = "true"))
+	USphereComponent* AmmoCollisionSphere;
+
+
+	/** 
+		Mesh
+	*/
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Ammo | Mesh", meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* AmmoMesh;
+
+	/**
+		Texture
+	*/
+	// Pickup Widget에서 총알의 타입에 맞게 Texture를 적용하기 위해
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ammo | Texture | Widget", meta = (AllowPrivateAccess = "true"))
+	UTexture2D* AmmoIconTexture;
+};
